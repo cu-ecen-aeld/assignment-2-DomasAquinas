f456570b7dff44c65ab42f561ec54d125492ded3
Domenic DiGiovanni <domenic.digiovanni@gmail.com>
2023-03-06 22:29:02 -0500

    Write writer.c

---
+++ finder-app/writer.c
@@ -0,0 +1,43 @@
+/**
+ * @file writer.c
+ * @author your name (you@domain.com)
+ * @brief writes to a file and keeps a log
+ */
+#include <stdio.h>
+#include <syslog.h>
+
+int main(int argc, char *argv[]) {
+
+    // Start the logging
+    openlog(NULL, LOG_CONS, LOG_USER);
+
+    // Check for at least two command line arguments
+    if (argc < 3) {
+        printf("Not enough arguments provided.");
+        return 1;
+    }
+
+    // Get the filename and string to write
+    const char * WRITEFILE = argv[1];
+    const char * WRITESTR = argv[2];
+
+    // Open the file, write the string, and close the file
+    syslog(LOG_DEBUG, "Writing %s to %s", WRITESTR, WRITEFILE);
+    int writeResult;
+    int closeResult;
+    FILE * writeFile;
+    writeFile = fopen(WRITEFILE, "a");
+    writeResult = fprintf(writeFile, WRITESTR);
+    closeResult = fclose(writeFile);
+
+    // Report an error if one occurred
+    if (writeResult <= 0) {
+        syslog(LOG_ERR, "File write unsuccessful.");
+        return 1;
+    }
+    if (closeResult == EOF) {
+        syslog(LOG_ERR, "File close unsuccessful.");
+        return 1;
+    }
+
+}
